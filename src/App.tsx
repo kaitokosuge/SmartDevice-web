8206cf518a43db399810716a294a24f699675d8f
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-04 15:16:01 +0900

    add: button

--- src/App.tsx
+++ src/App.tsx
@@ -3,85 +3,87 @@ import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
-	// CarouselNext,
-	// CarouselPrevious,
+	CarouselNext,
+	CarouselPrevious,
 } from "@/components/ui/carousel";
+import { type CarouselApi } from "@/components/ui/carousel";
+import React from "react";
 
 function App() {
+	const [api, setApi] = React.useState<CarouselApi>();
+	const [current, setCurrent] = React.useState(0);
+	const [count, setCount] = React.useState(0);
+
+	React.useEffect(() => {
+		if (!api) {
+			return;
+		}
+
+		setCount(api.scrollSnapList().length);
+		setCurrent(api.selectedScrollSnap() + 1);
+
+		api.on("select", () => {
+			setCurrent(api.selectedScrollSnap() + 1);
+		});
+	}, [api]);
 	return (
-		<div className="w-screen bg-gray-100 min-h-screen">
-			<Carousel>
-				<CarouselContent className="w-screen mx-auto h-[500px] text-[#1d1d1d] bg-gray-100 mt-20">
-					<CarouselItem className="bg-gray-100 w-full">
-						<div className="md:w-[80%] w-[70%] mx-auto grid grid-cols-2 gap-2">
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								vscode
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								vscode
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								vscode
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								vscode
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								vscode
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								vscode
-							</button>
-						</div>
-					</CarouselItem>
-					<CarouselItem className="bg-gray-100 w-full">
-						<div className="md:w-[80%] w-[70%] mx-auto grid grid-cols-2 gap-2">
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								slack
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								slack
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								slack
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								slack
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								slack
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								slack
-							</button>
-						</div>
-					</CarouselItem>
-					<CarouselItem className="bg-gray-100 w-full">
-						<div className="md:w-[80%] w-[70%] mx-auto grid grid-cols-2 gap-2">
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								notion
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								notion
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								notion
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								notion
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								notion
-							</button>
-							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
-								notion
-							</button>
-						</div>
-					</CarouselItem>
+		<div className="bg-gray-100 min-h-screen">
+			<Carousel
+				setApi={setApi}
+				opts={{
+					align: "start",
+					loop: true,
+				}}>
+				{Array.from({ length: 5 }).map((_, index) => (
+					<button
+						key={index}
+						onClick={() => {
+							api?.scrollTo(index);
+							setCurrent(index + 1);
+						}}>
+						{index + 1}
+					</button>
+				))}
+
+				<CarouselContent className="w-screen md:min-h-[600px] min-h-[500px] mx-auto text-[#1d1d1d] bg-gray-100 mt-20">
+					{Array.from({ length: 5 }).map((_, index) => (
+						<CarouselItem key={index} className="bg-gray-100 w-full">
+							<div className="md:w-[70%] w-[75%] mx-auto grid md:grid-cols-4 grid-cols-2 gap-2">
+								{index + 1}
+								<button className="block border p-2 border-[#b1b1b1] text-left rounded-[10px] font-bold aspect-square">
+									<p className="text-[#acacac]">vscode</p>
+									<p className="text-sm">新しいウィンドウを開く</p>
+								</button>
+								<button className="block border p-2 border-[#b1b1b1] text-left rounded-[10px] font-bold aspect-square">
+									<p className="text-[#acacac]">vscode</p>
+									<p className="text-sm">ウィンドウの再読み込み</p>
+								</button>
+								<button className="block border p-2 border-[#b1b1b1] text-left rounded-[10px] font-bold aspect-square">
+									<p className="text-[#acacac]">vscode</p>
+									<p className="text-sm">新しいターミナルを開く</p>
+								</button>
+								<button className="block border p-2 border-[#b1b1b1] text-left rounded-[10px] font-bold aspect-square">
+									<p className="text-[#acacac]">slack</p>
+									<p className="text-sm">channel「team-佐藤」を開く</p>
+								</button>
+								<button className="block border p-2 border-[#b1b1b1] text-left rounded-[10px] font-bold aspect-square">
+									<p className="text-[#acacac]">notion</p>
+									<p className="text-sm">新規ページを作成</p>
+								</button>
+								<button className="block border p-2 border-[#b1b1b1] text-left rounded-[10px] font-bold aspect-square">
+									<p className="text-[#acacac]">notion</p>
+									<p className="text-sm">「日報メモ」に子ページを作る</p>
+								</button>
+							</div>
+						</CarouselItem>
+					))}
 				</CarouselContent>
-				{/* <CarouselPrevious className="absolute left-1 text-gray-400" />
-				<CarouselNext /> */}
+				<CarouselPrevious className="absolute md:left-10 left-1 text-gray-400" />
+				<CarouselNext className="absolute md:right-10 right-1 text-gray-400" />
 			</Carousel>
+			<div className="py-2 text-center text-sm text-muted-foreground">
+				Slide {current} of {count}
+			</div>
 		</div>
 	);
 }
