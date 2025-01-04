b47d1ee13c3fe9647ce7867cae83f192082fc4a7
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-04 13:47:47 +0900

    add: ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,86 @@ import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
+	// CarouselNext,
+	// CarouselPrevious,
 } from "@/components/ui/carousel";
 
 function App() {
 	return (
-		<>
+		<div className="w-screen bg-gray-100 min-h-screen">
 			<Carousel>
-				<CarouselContent className="w-screen mx-auto h-[500px]">
-					<CarouselItem className="bg-gray-100 w-full">hello</CarouselItem>
-					<CarouselItem className="bg-gray-200 w-full">...</CarouselItem>
-					<CarouselItem className="bg-gray-300 w-full">...</CarouselItem>
+				<CarouselContent className="w-screen mx-auto h-[500px] text-[#1d1d1d] bg-gray-100 mt-20">
+					<CarouselItem className="bg-gray-100 w-full">
+						<div className="md:w-[80%] w-[70%] mx-auto grid grid-cols-2 gap-2">
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								vscode
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								vscode
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								vscode
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								vscode
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								vscode
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								vscode
+							</button>
+						</div>
+					</CarouselItem>
+					<CarouselItem className="bg-gray-100 w-full">
+						<div className="md:w-[80%] w-[70%] mx-auto grid grid-cols-2 gap-2">
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								slack
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								slack
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								slack
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								slack
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								slack
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								slack
+							</button>
+						</div>
+					</CarouselItem>
+					<CarouselItem className="bg-gray-100 w-full">
+						<div className="md:w-[80%] w-[70%] mx-auto grid grid-cols-2 gap-2">
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								notion
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								notion
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								notion
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								notion
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								notion
+							</button>
+							<button className="block border border-[#b1b1b1] rounded-[10px] font-bold aspect-square">
+								notion
+							</button>
+						</div>
+					</CarouselItem>
 				</CarouselContent>
-				{/* <CarouselPrevious />
+				{/* <CarouselPrevious className="absolute left-1 text-gray-400" />
 				<CarouselNext /> */}
 			</Carousel>
-		</>
+		</div>
 	);
 }
 
